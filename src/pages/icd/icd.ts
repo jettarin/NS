eb3051a14bb9997522e1370aa6fb725d26ce0f37
jettarin <bo_olk@outlook.com>
2018-07-23 23:20:59 +0700

    first commit

---
+++ src/pages/icd/icd.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Items } from '../../providers/providers';
+import { ImageViewerController } from "ionic-img-viewer";
+
+/**
+ * Generated class for the IcdPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-icd',
+  templateUrl: 'icd.html',
+})
+export class IcdPage {
+  item: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams,items: Items, public imageViewerCtrl: ImageViewerController) {
+    this.item = navParams.get('item')
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad IcdPage');
+  }
+
+}
